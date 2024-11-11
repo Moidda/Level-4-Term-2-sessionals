54e3fbfe0b6773c759a27960a1ee9b56460509ef
Mehedi Hasan <57999057+Moidda@users.noreply.github.com>
2024-11-12 01:05:33 +0600

    CNN

---
+++ CSE 472 Machine Learning Sessional/Conv2D.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+
+def getWindows(input, output_size, kernel_size, padding=0, stride=1, dilate=0):
+    working_input = input
+    working_pad = padding
+    # dilate the input if necessary
+    if dilate != 0:
+        working_input = np.insert(working_input, range(1, input.shape[2]), 0, axis=2)
+        working_input = np.insert(working_input, range(1, input.shape[3]), 0, axis=3)
+
+    # pad the input if necessary
+    if working_pad != 0:
+        working_input = np.pad(working_input, pad_width=((0,), (0,), (working_pad,), (working_pad,)), mode='constant', constant_values=(0.,))
+
+    in_b, in_c, out_h, out_w = output_size
+    out_b, out_c, _, _ = input.shape
+    batch_str, channel_str, kern_h_str, kern_w_str = working_input.strides
+
+    return np.lib.stride_tricks.as_strided(
+        working_input,
+        (out_b, out_c, out_h, out_w, kernel_size, kernel_size),
+        (batch_str, channel_str, stride * kern_h_str, stride * kern_w_str, kern_h_str, kern_w_str)
+    )
+
+
+class Conv2D:
+    """
+    An implementation of the convolutional layer. We convolve the input with out_channels different filters
+    and each filter spans all channels in the input.
+    """
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, weight=None, bias=None):
+        """
+        :param in_channels: the number of channels of the input data
+        :param out_channels: the number of channels of the output(aka the number of filters applied in the layer)
+        :param kernel_size: the specified size of the kernel(both height and width)
+        :param stride: the stride of convolution
+        :param padding: the size of padding. Pad zeros to the input with padding size.
+        """
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        self.cache = None
+
+        self._init_weights(weight, bias)
+
+    def _init_weights(self, weight, bias):
+        self.weight = 1e-3 * np.random.randn(self.out_channels, self.in_channels,  self.kernel_size, self.kernel_size)
+        self.bias = np.zeros(self.out_channels)
+        if weight is not None:
+            self.weight = weight
+        if bias is not None:
+            self.bias = bias
+
+    def forward(self, x):
+        """
+        The forward pass of convolution
+        :param x: input data of shape (N, C, H, W)
+        :return: output data of shape (N, self.out_channels, H', W') where H' and W' are determined by the convolution
+                 parameters.
+        """
+
+        n, c, h, w = x.shape
+        out_h = (h - self.kernel_size + 2 * self.padding) // self.stride + 1
+        out_w = (w - self.kernel_size + 2 * self.padding) // self.stride + 1
+
+        windows = getWindows(x, (n, c, out_h, out_w), self.kernel_size, self.padding, self.stride)
+
+        out = np.einsum('bihwkl,oikl->bohw', windows, self.weight)
+
+        # add bias to kernels
+        out += self.bias[None, :, None, None]
+
+        self.cache = x, windows
+        return out
+
+    def backward(self, dout):
+        """
+        The backward pass of convolution
+        :param dout: upstream gradients
+        :return: dx, dw, and db relative to this module
+        """
+        x, windows = self.cache
+
+        padding = self.kernel_size - 1 if self.padding == 0 else self.padding
+
+        dout_windows = getWindows(dout, x.shape, self.kernel_size, padding=padding, stride=1, dilate=self.stride - 1)
+        rot_kern = np.rot90(self.weight, 2, axes=(2, 3))
+
+        db = np.sum(dout, axis=(0, 2, 3))
+        dw = np.einsum('bihwkl,bohw->oikl', windows, dout)
+        dx = np.einsum('bohwkl,oikl->bihw', dout_windows, rot_kern)
+
+        return db, dw, dx
+
+
+in_channels = 3
+out_channels = 128
+kernel_size = 3
+stride = 2
+padding = 1
+batch_size = (4, in_channels, 12, 10)
+dout_size = (4, out_channels, 6, 5)
+
+np.random.seed(42)
+
+x = np.random.random(batch_size)  # create data for forward pass
+dout = np.random.random(dout_size)  # create random data for backward
+print('x: ', x.shape)
+print('d_out: ', dout.shape)
+
+conv = Conv2D(in_channels, out_channels, kernel_size, stride, padding)
+
+conv_out = conv.forward(x)
+print('conv_out: ', conv_out.shape)
+
+db, dw, dx = conv.backward(dout)
+print('db: ', db.shape)
+print('dw: ', dw.shape)
+print('dx: ', dx.shape)
